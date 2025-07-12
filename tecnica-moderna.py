dde0a498f6ccbe179500a9ee77e1c6fa95f6456c
João Victor Miranda <joaovictormiranda.99@gmail.com>
2025-07-11 23:15:56 -0300

    Changes in the modern technique and the LLM outputs after execution.

--- tecnica-moderna.py
+++ tecnica-moderna.py
@@ -1,28 +1,37 @@
-import re
-import pandas as pd
 from datasets import load_dataset
-from collections import deque
 from gpt4all import GPT4All
-
 
 ds = load_dataset("openai/graphwalks")
 ds = ds.with_format("pandas")
 
 df = ds["train"].to_pandas()
+output_LLM = open("output LLM - tecnica-moderna.txt", "a", encoding="utf-8")
+output_esperado = open("output esperado - tecnica-moderna.txt", "a", encoding="utf-8")
 
-model = GPT4All("Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf", device="nvidia", n_ctx=4096)
-lista_respostas = []
+ind = 1
 
-#print(model.generate(df["prompt"].iloc[0]))
+model = GPT4All("Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf", device="nvidia", n_ctx=16384)
 
 def resposta(row):
-
+    global ind
     resposta_llm = model.generate(row["prompt"])
+    ans_ini = "INICIO DA RESPOSTA: " + str(ind) + "\n\n"
+    output_LLM.write(ans_ini)
+    output_LLM.write(resposta_llm)
+    ans_end = "\n\nFIM DA RESPOSTA: " + str(ind) + "\n\n"
+    output_LLM.write(ans_end)
 
-    lista_respostas.append(resposta_llm)
+    resposta_esperada = "Final answer: " + str(row["answer_nodes"])
+    output_esperado.write(resposta_esperada)
+    output_esperado.write("\n")
 
-df_bfs = df[(df["problem_type"] == "bfs") & (df["prompt_chars"] <= 2700)].sample(n=10)
+    ind += 1
+
+df_bfs = df[(df["problem_type"] == "bfs") & (df["prompt_chars"] <= 10000)].head(115)
+df_parents = df[(df["problem_type"] == "parents") & (df["prompt_chars"] <= 10000)].head(115)
 
 df_bfs.apply(resposta, axis=1)
-print(lista_respostas)
+df_parents.apply(resposta, axis=1)
+
+
 
